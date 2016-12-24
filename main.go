56f4ff20af36a5bd31367a213d7cc5df3f919815
Tevin Mantock <tmantock@gmail.com>
2016-12-23 23:02:39 -0800

    using HandleFunc instead of Handle

--- main.go
+++ main.go
@@ -29,11 +29,11 @@ func main() {
 	//Serve static index page on root route
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	//Status route will be called to make sure the API is up and running
-	r.Handle("/status", NotImplemented).Methods("GET")
+	r.HandleFunc("/status", NotImplemented).Methods("GET")
 	//Producst route for retrieve a list of products user can leave feedback on
-	r.Handle("/products", NotImplemented).Methods("GET")
+	r.HandleFunc("/products", NotImplemented).Methods("GET")
 	//Producst/{slug}/feedback route will capture user feedback on products
-	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+	r.HandleFunc("/products/{slug}/feedback", NotImplemented).Methods("POST")
 	//Serve statics assets from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	//run the application on port 3000
@@ -41,6 +41,6 @@ func main() {
 }
 
 //NotImplemented handler handles whenever an API endpoint is hit it will return a not implemented message
-var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func NotImplemented (w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not implemented"))
-})
+}
