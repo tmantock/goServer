f747f2fb0bc99cc655dc2dfac4b6fad2eec78d27
Tevin Mantock <tmantock@gmail.com>
2016-12-23 22:37:18 -0800

    running go server

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
     r := mux.NewRouter()
 
     //Serve static index page on root route
-    r.Handle("/", http.FileServer(http.Dir("/views/")))
+    r.Handle("/", http.FileServer(http.Dir("./views/")))
     //Serve statics assets from the /static/{file} route
     r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
     //run the application on port 3000
