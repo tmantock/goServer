396e99a5c6f1c1db606acc52b2942187c199c422
Tevin Mantock <tmantock@gmail.com>
2016-12-23 23:25:42 -0800

    routes and handler functions set up

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"encoding/json"
 )
 
 //Product implements a new type of struct for information about VR experiences
@@ -29,11 +31,11 @@ func main() {
 	//Serve static index page on root route
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	//Status route will be called to make sure the API is up and running
-	r.HandleFunc("/status", NotImplemented).Methods("GET")
+	r.HandleFunc("/status", StatusHandler).Methods("GET")
 	//Producst route for retrieve a list of products user can leave feedback on
-	r.HandleFunc("/products", NotImplemented).Methods("GET")
+	r.HandleFunc("/products", ProductsHandler).Methods("GET")
 	//Producst/{slug}/feedback route will capture user feedback on products
-	r.HandleFunc("/products/{slug}/feedback", NotImplemented).Methods("POST")
+	r.HandleFunc("/products/{slug}/feedback", AddFeedbackHandler).Methods("POST")
 	//Serve statics assets from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	//run the application on port 3000
@@ -45,8 +47,41 @@ func NotImplemented (w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not implemented"))
 }
 
-//StatusHandler will be inkoked when a user makes a GETY request to the /products endpoint
-//it will return a list of products for the user to review
+//StatusHandler will be inkoked when a user makes a GETY request to the /status endpoint
+//it will simply return a string with the message relaying that the API is running
 func StatusHandler (w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("API is up and running"))
+}
+
+//ProductsHandler will be called when the user makes a GET request /products endpoint
+//it will return a list of products for the user to review
+func ProductsHandler (w http.ResponseWriter, r *http.Request) {
+    //conver the slice of Products to json
+    payload, _ := json.Marshal(products)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write([]byte(payload))
+}
+
+//AddFeedbackHandler will add either positive or negative feedbackto the products
+//For later save to database and send an OK status
+func AddFeedbackHandler (w http.ResponseWriter, r *http.Request) {
+    var product Product
+    vars := mux.Vars(r)
+    slug := vars["slug"]
+
+    for _, p := range products {
+        if p.Slug == slug {
+            product = p
+        }
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+
+    if product.Slug != "" {
+        payload, _ := json.Marshal(product)
+        w.Write([]byte(payload))
+    } else {
+        w.Write([]byte("Product Not Found"))
+    }
 }
